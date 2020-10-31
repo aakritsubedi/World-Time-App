d01de2f142eb356d5b9e304479000ad8b3ea4a0d
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-10-31 23:31:17 +0545

    Add a loading spinner in loading route

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:WorldClock/services/worldTime.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -7,16 +8,16 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
   void setupWorldTime() async {
-    WorldTime instance = WorldTime(location: 'Kathmandu', flag: 'nepal.png', url:'Asia/Kathmandu');
+    WorldTime instance = WorldTime(
+        location: 'Kathmandu', flag: 'nepal.png', url: 'Asia/Kathmandu');
     await instance.getTime();
 
     // Redirect to other routes
     Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location': instance.location,
       'time': instance.time,
-      'flag': instance.flag 
+      'flag': instance.flag
     });
   }
 
@@ -30,11 +31,10 @@ class _LoadingState extends State<Loading> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Text('Loading'),
-        )
-      ),
+          child: SpinKitChasingDots(
+        color: Colors.grey[400],
+        size: 50.0,
+      )),
     );
   }
 }
